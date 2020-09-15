ea3f9d72fc945844cc4362776c1642abfecb5e22
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-15 07:45:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,13 +22,14 @@ def output_array(array)
   end
 end
 
-`def return_string_array(array)
+def return_string_array(array)
   count = 0 
   while count < array.length do
-   array[count] = array [count].to_s 
-   count += 1
+    array[count] = array[count].to_s 
+    count += 1 
   end
-end`
+  array
+end
   
   
   
